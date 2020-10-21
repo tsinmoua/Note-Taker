d16a30a04ac819ea5001389184793fc94c1fbda9
Tsin Moua <tsinmoua@gmail.com>
2020-10-20 20:12:16 -0500

    Added get db.json

--- server.js
+++ server.js
@@ -7,20 +7,17 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
-    console.log(`/notes on http://localhost:${PORT}/notes`);
-
 });
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
-
-app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "db.json"));
-    console.log(`/api/notes on http://localhost:${PORT}/api/notes`);
-  });
 
 
 app.listen(PORT, function() {
